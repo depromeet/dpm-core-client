ab7af5ef9732bd8719d608ce1faa65d99f1830e1
raymondanythings <raymondanything@gmail.com>
2025-06-26 00:23:27 +0900

    chore: custom server 연동을 위한 generic oauth 설정

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,18 @@
 import { betterAuth } from 'better-auth';
+import { genericOAuth } from 'better-auth/plugins';
 
-export const auth = betterAuth({});
+export const auth = betterAuth({
+	plugins: [
+		genericOAuth({
+			config: [
+				{
+					clientId: '',
+					clientSecret: '',
+					providerId: '',
+					// getUserInfo(tokens) {},
+					// mapProfileToUser(profile) {},
+				},
+			],
+		}),
+	],
+});
