791d77d34c09d4e28f2937c61f0653961f449da2
raymondanythings <raymondanything@gmail.com>
2025-07-19 22:11:37 +0900

    feat: API 및 백엔드 시스템 개선
    - 인증 API 원격 통신 로직 개선
    - HTTP 클라이언트 설정 업데이트
    - 멤버 API 및 타입 정의 개선

--- packages/api/src/member/remote.ts
+++ packages/api/src/member/remote.ts
@@ -6,4 +6,9 @@ export const member = {
 		const res = await http.get<Member>('v1/members/me');
 		return res;
 	},
+
+	withdraw: async () => {
+		const res = await http.patch('v1/members/withdraw');
+		return res;
+	},
 };
