622bf0200f4f09fbf27181ef825e758364dbb751
raymondanythings <raymondanything@gmail.com>
2025-07-20 03:51:16 +0900

    refactor: 사용하지 않는 파일들 및 레이아웃 정리
    - 어드민 (user) 라우트 관련 파일들 삭제
    - 클라이언트 레이아웃 파일들 정리
    - 사용하지 않는 세션 배너 컴포넌트 삭제

--- apps/client/layout/unauthenticated-layout.tsx
+++
@@ -1,7 +0,0 @@
-import { RedirectType, redirect } from 'next/navigation';
-
-function UnauthenticatedLayout() {
-	return redirect('/login', RedirectType.replace);
-}
-
-export { UnauthenticatedLayout };
