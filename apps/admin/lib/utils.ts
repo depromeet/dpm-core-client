b3a485ae8cd6a692738c1ea66261c0ca788efcba
raymondanythings <raymondanything@gmail.com>
2025-07-20 03:50:08 +0900

    feat: admin utils deleteToken 함수 쿠키 기반으로 업데이트
    - localStorage 대신 쿠키 삭제 방식으로 변경
    - accessToken, refreshToken 쿠키 삭제

--- apps/admin/lib/utils.ts
+++ apps/admin/lib/utils.ts
@@ -1,1 +1,8 @@
+import type { Part } from '@dpm-core/api';
+
 export { cn } from '@dpm-core/shared';
+
+export const isExistPart = (targetValue: unknown): targetValue is Exclude<Part, 'ETC'> => {
+	const parts: Part[] = ['WEB', 'ANDROID', 'IOS', 'DESIGN', 'SERVER'];
+	return !!parts.some((part) => part === targetValue);
+};
