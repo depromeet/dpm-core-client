01a1a44e812376d5bf210b0277ec89efc6d86a65
leejeongho <jh9628771@gmail.com>
2025-07-21 06:49:10 +0900

    feat: 세션 출석 시간 조회 API 추가

--- apps/client/remotes/queries/session.ts
+++ apps/client/remotes/queries/session.ts
@@ -5,3 +5,10 @@ export const getSessionListQuery = queryOptions({
 	queryKey: ['session-list'],
 	queryFn: session.getList,
 });
+
+export const getSessionAttendanceTimeOptions = (sessionId: number) =>
+	queryOptions({
+		queryKey: ['sessions', 'attendance-time'],
+		queryFn: () => session.getSessionAttendanceTime(sessionId),
+		retry: false,
+	});
