daeb70a9569da38924059367fb06fa0a7ba1afe5
leejeongho <jh9628771@gmail.com>
2025-07-21 05:04:04 +0900

    feat: toast error 생성

--- packages/shared/src/components/ui/toast.tsx
+++ packages/shared/src/components/ui/toast.tsx
@@ -1,5 +1,5 @@
 import { type ExternalToast, toast as toastify } from 'sonner';
-import { CheckIcon } from '../icons';
+import { CheckIcon, CircleAlert } from '../icons';
 
 interface ToastProps {
 	toastId: string | number;
@@ -18,9 +18,21 @@ const Toast = ({ toastId, message }: ToastProps) => {
 	);
 };
 
+const ErrorToast = ({ toastId, message }: ToastProps) => {
+	return (
+		<div className="flex items-center py-3 px-4 rounded-full bg-gray-800/90 w-fit mx-auto gap-x-2 pointer-events-none select-none">
+			<CircleAlert />
+			<p className="text-body2 font-medium text-neutral-5">{message}</p>
+		</div>
+	);
+};
+
 const toast = {
 	success: (message: string, options?: ExternalToast) => {
 		toastify.custom((id) => <Toast toastId={id} message={message} />, options);
+	},
+	error: (message: string, options?: ExternalToast) => {
+		toastify.custom((id) => <ErrorToast toastId={id} message={message} />, options);
 	},
 };
 
