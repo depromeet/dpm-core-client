09229e4db63ba3de3b053947db12088585aa23a0
raymondanythings <raymondanything@gmail.com>
2025-07-21 00:50:01 +0900

    feat: admin 앱 세션 페이지 컴포넌트 추가
    - 세션 상세 정보 컴포넌트 추가
    - 세션 상세 페이지 구현
    - 세션 리스트 컴포넌트 추가
    - 세션 메인 페이지 구현

---
+++ apps/admin/app/(auth)/session/page.tsx
@@ -0,0 +1,15 @@
+import { AppLayout } from '@dpm-core/shared';
+import { AppHeader } from '@/components/app-header';
+import { SessionList } from './_components/session-list';
+
+const SessionPage = () => {
+	return (
+		<AppLayout className="bg-background-normal ">
+			<AppHeader title="세션" backHref="/" className="mb-1.5" />
+
+			<SessionList />
+		</AppLayout>
+	);
+};
+
+export default SessionPage;
