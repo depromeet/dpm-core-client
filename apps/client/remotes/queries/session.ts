789c176235f82bc0badc9aca8af77fa9cc437f08
leejeongho <92032081+LeeJeongHooo@users.noreply.github.com>
2025-07-22 19:49:05 +0900

    fix: 디퍼 홈 화면 다음 세션 조회 수정 (#10)
    * fix: Home화면 API 수정
    
    * fix: Link 수정

--- apps/client/remotes/queries/session.ts
+++ apps/client/remotes/queries/session.ts
@@ -1,10 +1,19 @@
 import { session } from '@dpm-core/api';
+
 import { queryOptions } from '@tanstack/react-query';
 
 export const getSessionListQuery = queryOptions({
 	queryKey: ['session-list'],
 	queryFn: session.getList,
 });
+
+export const getSessionCurrentOptions = () =>
+	queryOptions({
+		queryKey: ['session-current-week'],
+		queryFn: async () => {
+			return session.getCurrentWeekSession();
+		},
+	});
 
 export const getSessionAttendanceTimeOptions = (sessionId: number) =>
 	queryOptions({
