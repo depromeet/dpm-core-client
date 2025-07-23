bf17382ca8e4ea69e344f6c1d657a48d139f5cf9
leejeongho <jh9628771@gmail.com>
2025-07-23 23:01:47 +0900

    feat: 어드민 출석 api 추가

---
+++ apps/admin/remotes/mutations/attendance.ts
@@ -0,0 +1,28 @@
+import { type AttendanceStatus, attendance } from '@dpm-core/api';
+import { type MutationOptions, mutationOptions } from '@tanstack/react-query';
+
+const MUTATE_KEY = 'ATTENDANCE';
+
+interface ModifyAttendanceStatusParams {
+	attendanceStatus: AttendanceStatus;
+}
+
+type ModifyAttendanceStatusOptions = MutationOptions<
+	unknown,
+	Error,
+	ModifyAttendanceStatusParams,
+	unknown
+>;
+
+/* 출석 상태 변경 */
+export const modifyAttendanceStatusOptions = (
+	sessionId: number,
+	memberId: number,
+	options?: ModifyAttendanceStatusOptions,
+) =>
+	mutationOptions({
+		mutationKey: [MUTATE_KEY, sessionId],
+		mutationFn: (params: ModifyAttendanceStatusParams) =>
+			attendance.modifyAttendanceStatus({ sessionId, memberId, ...params }),
+		...options,
+	});
