665c1bb54b23d85d23e60c598e31ba9cceba6b9c
raymondanythings <raymondanything@gmail.com>
2025-08-06 01:15:04 +0900

    feat(FQA-136): 어드민 홈 ui 스타일 변경

--- packages/shared/src/components/icons/user-avatar.tsx
+++ packages/shared/src/components/icons/user-avatar.tsx
@@ -3,46 +3,46 @@ import { type ComponentPropsWithoutRef, forwardRef } from 'react';
 const UserAvatar = forwardRef<SVGSVGElement, ComponentPropsWithoutRef<'svg'>>((props, ref) => {
 	return (
 		<svg
-			ref={ref}
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
+			width="32"
+			height="32"
+			viewBox="0 0 32 32"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
+			ref={ref}
 			{...props}
 		>
-			<title>Dpmcore(Logo)</title>
-			<g clipPath="url(#clip0_1250_4198)">
-				<rect width="24" height="24" rx="12" fill="#F3F4F6" />
-				<g clipPath="url(#clip1_1250_4198)">
+			<title>userAvatar</title>
+			<g clip-path="url(#clip0_2839_3758)">
+				<rect x="4" y="4" width="24" height="24" rx="12" fill="#F3F4F6" />
+				<g clip-path="url(#clip1_2839_3758)">
 					<path
-						d="M11.7318 12.4591C13.7151 12.4591 15.3229 10.8513 15.3229 8.86799C15.3229 6.88466 13.7151 5.27686 11.7318 5.27686C9.74843 5.27686 8.14062 6.88466 8.14062 8.86799C8.14062 10.8513 9.74843 12.4591 11.7318 12.4591Z"
+						d="M15.7318 16.4586C17.7151 16.4586 19.3229 14.8508 19.3229 12.8675C19.3229 10.8842 17.7151 9.27637 15.7318 9.27637C13.7484 9.27637 12.1406 10.8842 12.1406 12.8675C12.1406 14.8508 13.7484 16.4586 15.7318 16.4586Z"
 						fill="#9CA3AF"
 						stroke="#9CA3AF"
-						strokeWidth="1.59606"
-						strokeLinecap="round"
-						strokeLinejoin="round"
+						stroke-width="1.59606"
+						stroke-linecap="round"
+						stroke-linejoin="round"
 					/>
 					<path
-						d="M19.4483 21.9349C19.4483 23.3897 18.5173 23.898 12 23.898C5.48279 23.898 4.55176 23.3897 4.55176 21.9349C4.55176 20.4802 5.33648 19.085 6.73331 18.0563C8.13013 17.0276 10.0246 16.4497 12 16.4497C13.9754 16.4497 15.8699 17.0276 17.2668 18.0563C18.6636 19.085 19.4483 20.4802 19.4483 21.9349Z"
+						d="M23.4478 25.9347C23.4478 27.3895 22.5168 27.8977 15.9995 27.8977C9.4823 27.8977 8.55127 27.3895 8.55127 25.9347C8.55127 24.4799 9.336 23.0847 10.7328 22.0561C12.1296 21.0274 14.0241 20.4495 15.9995 20.4495C17.975 20.4495 19.8694 21.0274 21.2663 22.0561C22.6631 23.0847 23.4478 24.4799 23.4478 25.9347Z"
 						fill="#9CA3AF"
 						stroke="#9CA3AF"
-						strokeWidth="1.59606"
-						strokeLinecap="round"
-						strokeLinejoin="round"
+						stroke-width="1.59606"
+						stroke-linecap="round"
+						stroke-linejoin="round"
 					/>
 				</g>
 			</g>
 			<defs>
-				<clipPath id="clip0_1250_4198">
-					<rect width="24" height="24" rx="12" fill="white" />
+				<clipPath id="clip0_2839_3758">
+					<rect x="4" y="4" width="24" height="24" rx="12" fill="white" />
 				</clipPath>
-				<clipPath id="clip1_1250_4198">
+				<clipPath id="clip1_2839_3758">
 					<rect
 						width="22.3448"
 						height="22.3448"
 						fill="white"
-						transform="translate(0.827148 2.48291)"
+						transform="translate(4.82715 6.48267)"
 					/>
 				</clipPath>
 			</defs>
