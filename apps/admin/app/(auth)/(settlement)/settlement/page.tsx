f945f9186ea07680cd5bfeacbfe127ad5b4bb284
leejeongho <jh9628771@gmail.com>
2025-08-24 00:35:36 +0900

    remove: 기존 settle 파일 삭제

--- apps/admin/app/(auth)/(settlement)/settlement/page.tsx
+++
@@ -1,9 +0,0 @@
-import MainTemplete from '@/components/settle/templates/MainTemplete';
-
-export default function SettleMainPage() {
-	return (
-		<div className="px-4">
-			<MainTemplete />
-		</div>
-	);
-}
