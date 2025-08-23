bf4011624f511d2f87e2ae613a021c186e6546e0
leejeongho <jh9628771@gmail.com>
2025-08-24 00:38:05 +0900

    feat: 멤버별 최종 정산 기능 추가

---
+++ apps/admin/app/(auth)/bills/[billId]/(status)/final-amount/_components/final-amount-by-member-item.tsx
@@ -0,0 +1,23 @@
+import type { Part } from '@dpm-core/api';
+import { Profile } from '@/components/attendance/profile';
+
+interface FinalAmountByMemberItemProps {
+	member: {
+		name: string;
+		teamNumber: number;
+		authority: string;
+		part: Exclude<Part, 'ETC'>;
+		splitAmount: number;
+	};
+}
+export const FinalAmountByMemberItem = ({ member }: FinalAmountByMemberItemProps) => {
+	return (
+		<div className="flex items-center justify-between">
+			<Profile name={member.name} teamNumber={member.teamNumber} part={member.part} />
+			<p className="flex gap-1 text-body2 font-semibold">
+				<span className="text-label-normal"></span>
+				<span className="text-label-assistive">원</span>
+			</p>
+		</div>
+	);
+};
