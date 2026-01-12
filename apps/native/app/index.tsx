7a579c638277da268e0e4d9fd812ce6df2dfd20b
leejeongho <jh9628771@gmail.com>
2026-01-13 05:44:47 +0900

    feat: Rn 웹뷰 초기 세팅

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,15 +1,48 @@
-import { Text, View } from "react-native";
+import { useEffect, useRef, useState } from 'react';
+import { BackHandler, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import WebView, { type WebViewNavigation } from 'react-native-webview';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function Home() {
+	const webViewRef = useRef<WebView>(null);
+	const insets = useSafeAreaInsets();
+	const [canGoBack, setCanGoBack] = useState(false);
+
+	useEffect(() => {
+		const onBackPress = () => {
+			if (canGoBack && webViewRef.current) {
+				webViewRef.current.goBack();
+				return true;
+			}
+			return false;
+		};
+
+		const backHandler = BackHandler.addEventListener('hardwareBackPress', onBackPress);
+		return () => backHandler.remove();
+	}, [canGoBack]);
+
+	const handleNavigationChnage = (navState: WebViewNavigation) => {
+		setCanGoBack(navState.canGoBack);
+	};
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				paddingTop: insets.top,
+				paddingBottom: insets.bottom,
+			}}
+		>
+			<WebView
+				style={{
+					flex: 1,
+				}}
+				ref={webViewRef}
+				source={{ uri: 'https://core.depromeet.com' }}
+				onNavigationStateChange={handleNavigationChnage}
+				overScrollMode="never"
+				sharedCookiesEnabled={true}
+			/>
+		</View>
+	);
 }
